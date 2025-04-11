aed277f39b5c0d99bccaaa3854bbc700abfb42f2
ChuJordan <jor.chu@mail.utoronto.ca>
2025-04-11 11:20:37 -0400

    manifest, background files, css

---
+++ copier.js
@@ -0,0 +1,43 @@
+const katexCopyEL = "katex-display";
+
+function addLatexListeners() {
+    const katexEls = document.getElementsByClassName(katexCopyEL);
+
+    let prevCount = katexEls.length;
+
+    const observer = new MutationObserver(() => {
+        const newCount = katexEls.length;
+        if (newCount !== prevCount) {
+            prevCount = newCount;
+
+            Array.from(katexEls).forEach((katexEl) => {
+                katexEl.classList.add("custom-latex-display");
+                katexEl.setAttribute("margin", "0");
+                katexEl.setAttribute("padding", "1em 0");
+                katexEl.addEventListener("click", () => {
+                    katexEl.classList.remove("copied");
+                    console.log("copied");
+                    const latex =
+                        katexEl.getElementsByTagName("annotation")[0].innerHTML;
+                    navigator.clipboard.writeText(latex);
+
+                    // animation for copy
+                    katexEl.classList.add("copied");
+                    katexEl.addEventListener("animationend", () => {
+                        katexEl.classList.remove("copied");
+                    });
+                });
+            });
+        }
+    });
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+    });
+}
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", addLatexListeners);
+} else {
+    addLatexListeners();
+}
